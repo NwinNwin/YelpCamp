4bdf094a9c8a827c782c1c01faa62f5b9fd2955e
NwinNwin <ndnguyen2003@gmail.com>
2022-09-13 13:44:00 -0700

    try with fake db

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp")
+  .then(() => {
+    console.log("Mongo Connection Open");
+  })
+  .catch((err) => {
+    console.log("ERROR!");
+    console.log(err);
+  });
+
+// return random element from the array (using length)
+const sample = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+//close connection
+seedDB().then(() => {
+  mongoose.connection.close();
+});
