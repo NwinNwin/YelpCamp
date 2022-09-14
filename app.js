4feba765856f524935f08dfd40e41cf36c84aa67
NwinNwin <ndnguyen2003@gmail.com>
2022-09-13 20:30:57 -0700

    implement full mongoDB CRUD

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+var methodOverride = require("method-override");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp")
@@ -14,6 +15,9 @@ mongoose
     console.log(err);
   });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -21,10 +25,48 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({ title: "westminster", description: "nice camp" });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const camps = await Campground.find({});
+  res.render("campgrounds/index", { camps });
+});
+
+//new camp route
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const newCampground = new Campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+});
+
+//show detail route
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const camp = await Campground.findById(id);
+  res.render("campgrounds/show", { camp });
+});
+
+//edit route
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const camp = await Campground.findById(id);
+  res.render("campgrounds/edit", { camp });
+});
+
+//edit put route
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true });
+  res.redirect(`/campgrounds/${id}`);
+});
+
+//delete route
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
